08c45e98e3955e27c0f49e79c9b3530ed835fea4
zihe <he100zihao100@126.com>
2020-07-23 12:33:40 +0800

    add: 增加zuul网关及其配置

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/atguigu/springcloud/Zuul9527StarSpringCloudApp.java
@@ -0,0 +1,22 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author hezihao
+ * @version 1.0
+ * <p>
+ * @date 2020/7/23 11:45 上午
+ * <p>
+ * 网关
+ */
+@SpringBootApplication
+//标识为Zuul网关
+@EnableZuulProxy
+public class Zuul9527StarSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul9527StarSpringCloudApp.class, args);
+    }
+}
